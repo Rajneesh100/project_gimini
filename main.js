4dd261aeb37b0b05ce77ae062b7390d50bf329fb
Rajneesh100 <rajneesh_2001cs53@iitp.ac.in>
2024-09-07 14:47:24 +0530

    opening the server for all ports and ips

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 const db= require('./db')
 const PORT= process.env.PORT
 
-
+app.use(cors({
+    origin: '*', // Allow this origin for development
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+  }));
 
 
 app.get('/', (req,res)=>{
